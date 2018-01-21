e5daf376cc12a7463b91438f046107039e4edb38
Alain Martin <amartin@keytrol.org>
2018-01-21 03:42:15 +0100

    Skip work if the target exists

--- cmake/Modules/FindJUCE-find-module.cmake
+++ cmake/Modules/FindJUCE-find-module.cmake
@@ -15,24 +15,26 @@
 # You should have received a copy of the GNU General Public License
 # along with FRUT.  If not, see <http://www.gnu.org/licenses/>.
 
+if(TARGET JUCE::${_this_module})
+  set(${_this_module}_FOUND TRUE)
+  return()
+endif()
+
 set(${_this_module}_NOT_FOUND_REASON)
 
-if(NOT TARGET JUCE::${_this_module})
-  find_file(${_this_module}_header "${_this_module}/${_this_module}.h"
-    HINTS "${JUCE_ROOT}/modules" "${JUCE_MODULES_DIR}"
+find_file(${_this_module}_header "${_this_module}/${_this_module}.h"
+  HINTS "${JUCE_ROOT}/modules" "${JUCE_MODULES_DIR}"
+)
+
+if(NOT ${_this_module}_header)
+  string(APPEND ${_this_module}_NOT_FOUND_REASON
+    "\nUnable to find the ${_this_module} header file."
+    " Please set JUCE_ROOT to the root directory containing JUCE or"
+    " set JUCE_MODULES_DIR to the directory containing JUCE's modules."
   )
-
-  if(NOT ${_this_module}_header)
-    string(APPEND ${_this_module}_NOT_FOUND_REASON
-      "\nUnable to find the ${_this_module} header file."
-      " Please set JUCE_ROOT to the root directory containing JUCE or"
-      " set JUCE_MODULES_DIR to the directory containing JUCE's modules."
-    )
-
-  else()
-    include("${CMAKE_CURRENT_LIST_DIR}/FindJUCE-helpers.cmake")
-    _FRUT_add_target_from_module_header("${${_this_module}_header}" ${_this_module})
-  endif()
+else()
+  include("${CMAKE_CURRENT_LIST_DIR}/FindJUCE-helpers.cmake")
+  _FRUT_add_target_from_module_header("${${_this_module}_header}" ${_this_module})
 endif()
 
 if(${_this_module}_NOT_FOUND_REASON)
--- cmake/Modules/Findjuce_core.cmake
+++ cmake/Modules/Findjuce_core.cmake
@@ -15,43 +15,45 @@
 # You should have received a copy of the GNU General Public License
 # along with FRUT.  If not, see <http://www.gnu.org/licenses/>.
 
+if(TARGET JUCE::juce_core)
+  set(juce_core_FOUND TRUE)
+  return()
+endif()
+
 set(juce_core_NOT_FOUND_REASON)
 
-if(NOT TARGET JUCE::juce_core)
-  find_file(juce_core_header "juce_core/juce_core.h"
-    HINTS "${JUCE_ROOT}/modules" "${JUCE_MODULES_DIR}"
-  )
+find_file(juce_core_header "juce_core/juce_core.h"
+  HINTS "${JUCE_ROOT}/modules" "${JUCE_MODULES_DIR}"
+)
 
-  if(NOT juce_core_header)
-    string(APPEND juce_core_NOT_FOUND_REASON
-      "\nUnable to find the juce_core header file."
-      " Please set JUCE_ROOT to the root directory containing JUCE or"
-      " set JUCE_MODULES_DIR to the directory containing JUCE's modules."
+if(NOT juce_core_header)
+  string(APPEND juce_core_NOT_FOUND_REASON
+    "\nUnable to find the juce_core header file."
+    " Please set JUCE_ROOT to the root directory containing JUCE or"
+    " set JUCE_MODULES_DIR to the directory containing JUCE's modules."
+  )
+else()
+  include("${CMAKE_CURRENT_LIST_DIR}/FindJUCE-helpers.cmake")
+  _FRUT_add_target_from_module_header("${juce_core_header}" juce_core)
+
+  if(TARGET JUCE::juce_core)
+    string(CONCAT _define_JUCE_STANDALONE_APPLICATION
+      "JUCE_STANDALONE_APPLICATION=$<OR:"
+        "$<STREQUAL:$<TARGET_PROPERTY:TYPE>,EXECUTABLE>,"
+        "$<STREQUAL:$<TARGET_PROPERTY:TYPE>,MODULE_LIBRARY>"
+      ">"
+    )
+    set_property(TARGET JUCE::juce_core APPEND PROPERTY
+      INTERFACE_COMPILE_DEFINITIONS
+      "JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED"
+      "${_define_JUCE_STANDALONE_APPLICATION}"
     )
 
-  else()
-    include("${CMAKE_CURRENT_LIST_DIR}/FindJUCE-helpers.cmake")
-    _FRUT_add_target_from_module_header("${juce_core_header}" juce_core)
-
-    if(TARGET JUCE::juce_core)
-      string(CONCAT _define_JUCE_STANDALONE_APPLICATION
-        "JUCE_STANDALONE_APPLICATION=$<OR:"
-          "$<STREQUAL:$<TARGET_PROPERTY:TYPE>,EXECUTABLE>,"
-          "$<STREQUAL:$<TARGET_PROPERTY:TYPE>,MODULE_LIBRARY>"
-        ">"
-      )
-      set_property(TARGET JUCE::juce_core APPEND PROPERTY
-        INTERFACE_COMPILE_DEFINITIONS
-        "JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED"
-        "${_define_JUCE_STANDALONE_APPLICATION}"
-      )
-
-      get_filename_component(juce_core_dir "${juce_core_header}" DIRECTORY)
-      get_filename_component(juce_modules_dir "${juce_core_dir}" DIRECTORY)
-      set_property(TARGET JUCE::juce_core APPEND PROPERTY
-        INTERFACE_INCLUDE_DIRECTORIES "${juce_modules_dir}"
-      )
-    endif()
+    get_filename_component(juce_core_dir "${juce_core_header}" DIRECTORY)
+    get_filename_component(juce_modules_dir "${juce_core_dir}" DIRECTORY)
+    set_property(TARGET JUCE::juce_core APPEND PROPERTY
+      INTERFACE_INCLUDE_DIRECTORIES "${juce_modules_dir}"
+    )
   endif()
 endif()
 
